c5a1aa3a963106ad1b055562eb3d2d9c6bc421c1
Shin <131666565+Juniorlearn13-9-6-24-23@users.noreply.github.com>
2023-10-15 14:46:03 +0700

    update

--- flappy-bird.js
+++ flappy-bird.js
@@ -1,6 +1,26 @@
 const canvasEl = document.getElementById('canvas');
 const ctx = canvasEl.getContext('2d');
 
+let pipes = [
+    {
+        x: 290,
+        h: 150,
+        d: 70,
+    },
+];
+
+// 52 laf chiều rộng ống
+function addRandomPipe() {
+    let x =
+        pipes[pipes.length - 1].x + 52 + Math.floor(Math.random() * 30 + 40);
+    let d = Math.floor(Math.random() * 25 + 35);
+    let maxh = 400 - d - 30;
+    let h = Math.floor(Math.random() * (maxh - 30) + 30);
+}
+
+for (let i = 0; i < 4; i++) {
+    addRandomPipe();
+}
 pipe = {
     x: 130,
     h: 150,
@@ -11,6 +31,7 @@ const bird = {
     x: 3,
     y: 491,
     id: 0,
+    id: 100,
 };
 
 let lastTimeUpdate = 0;
@@ -37,6 +58,7 @@ function drawStartScreen() {
     // h là đích
     // Bird
     ctx.drawImage(img, bird.x, bird.y, 17, 12, 144 - 17, 150, 34, 24);
+
     // Land
     ctx.save();
     ctx.translate(-landShift, 0);
@@ -45,6 +67,7 @@ function drawStartScreen() {
     ctx.drawImage(img, 292, 0, 168, 56, 235, 400, 236, 112);
 
     ctx.restore();
+
     // Start
     ctx.drawImage(img, 354, 118, 52, 29, 20, 342, 104, 58);
     // Rank
@@ -94,10 +117,75 @@ function updateStartScreen() {
     backGround = (backGround + 1) % 184;
 }
 
+// Màn hình bắt đầu
+function drawPlayingScreen() {
+    // Bg
+    ctx.save();
+    ctx.translate(-backGround, 0);
+    ctx.drawImage(img, 0, 0, 144, 256, 0, 0, 288, 512);
+    ctx.drawImage(img, 0, 0, 144, 256, 235, 0, 288, 512);
+    ctx.restore();
+    // ctx.drawImage(img, xs, ys, ws, hs, xd, yd, wd, hd);
+    // s là gốc
+    // h là đích
+    // Bird
+    ctx.drawImage(img, bird.x, bird.y, 17, 12, 60, 180, 34, 24);
+
+    // Land
+    ctx.save();
+    ctx.translate(-landShift, 0);
+
+    ctx.drawImage(img, 292, 0, 168, 56, 0, 400, 236, 112);
+    ctx.drawImage(img, 292, 0, 168, 56, 235, 400, 236, 112);
+
+    ctx.restore();
+
+    // Draw pipe
+    ctx.drawImage(img, 56, 323, 26, 160, pipe.x, 0, 52, pipe.h);
+    ctx.drawImage(
+        img,
+        84,
+        323,
+        26,
+        160,
+        pipe.x,
+        pipe.h + pipe.d,
+        52,
+        400 - pipe.h - pipe.d
+    );
+}
+
+function updatePlayingScreen() {
+    if (lastTimeUpdate === 0) lastTimeUpdate = Date.now();
+    if (Date.now() - lastTimeUpdate >= 1000 / pageFlipV) {
+        bird.id = (bird.id + 1) % 3;
+        switch (bird.id) {
+            case 0:
+                bird.x = 87;
+                bird.y = 491;
+                break;
+            case 1:
+                bird.x = 115;
+                bird.y = 329;
+                break;
+            case 2:
+                bird.x = 115;
+                bird.y = 355;
+                break;
+        }
+        lastTimeUpdate = Date.now();
+    }
+
+    landShift = (landShift + 1) % 184;
+    backGround = (backGround + 1) % 184;
+}
+
 function animation() {
     clearScreen();
-    drawStartScreen();
-    updateStartScreen();
+    // drawStartScreen();
+    // updateStartScreen();
+    drawPlayingScreen();
+    updatePlayingScreen();
 
     requestAnimationFrame(animation);
 }
